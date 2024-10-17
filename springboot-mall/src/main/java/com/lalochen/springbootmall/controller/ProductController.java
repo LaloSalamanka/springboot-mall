b985f9b0365cea3bf9a7807039d9c39aa5a37cd7
zon <zonchen1925@gmail.com>
2024-10-17 20:23:20 +0800

    商品功能 - 實作刪除商品

--- springboot-mall/src/main/java/com/lalochen/springbootmall/controller/ProductController.java
+++ springboot-mall/src/main/java/com/lalochen/springbootmall/controller/ProductController.java
@@ -51,5 +51,10 @@ public class ProductController {
         return ResponseEntity.status(HttpStatus.OK).body(updatedProduct);
     }
 
+    @DeleteMapping("/products/{productId}")
+    public ResponseEntity<Product> deleteProduct(@PathVariable Integer productId) {
+        productService.deleteProductById(productId);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
 
 }
